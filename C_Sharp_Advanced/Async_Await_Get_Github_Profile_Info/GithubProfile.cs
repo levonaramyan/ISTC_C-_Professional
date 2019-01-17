a2ae71f19fe74619a2c6af6d3662ac487b58ecb5
levonaramyan <levonaramyan@gmail.com>
2019-01-17 19:17:27 +0400

    New Project: Get and print the github profile information asynchronously (using async/await).

---
+++ C_Sharp_Advanced/Async_Await_Get_Github_Profile_Info/GithubProfile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Async_Await_Get_Github_Profile_Info
+{
+    public class GithubProfile
+    {
+        public string login { get; set; }
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string avatar_url { get; set; }
+        public string gravatar_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public string followers_url { get; set; }
+        public string following_url { get; set; }
+        public string gists_url { get; set; }
+        public string starred_url { get; set; }
+        public string subscriptions_url { get; set; }
+        public string organizations_url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string received_events_url { get; set; }
+        public string type { get; set; }
+        public bool site_admin { get; set; }
+        public string name { get; set; }
+        public string company { get; set; }
+        public string blog { get; set; }
+        public string location { get; set; }
+        public object email { get; set; }
+        public object hireable { get; set; }
+        public string bio { get; set; }
+        public int public_repos { get; set; }
+        public int public_gists { get; set; }
+        public int followers { get; set; }
+        public int following { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
---
+++ C_Sharp_Advanced/Async_Await_Get_Github_Profile_Info/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Async_Await_Get_Github_Profile_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isImplemented = TryStartToEndAsync().Result;
+            if (!isImplemented) Console.WriteLine("We were unable to obtain data for current user.");
+            Console.ReadKey();
+        }
+
+        static async Task<bool> TryStartToEndAsync()
+        {
+            try
+            {
+                string url = @"https://api.github.com/users/" + await ReadUsernameFromConsoleAsync();
+                string userJson = await GetDataFromURLAsync(url);
+                GithubProfile user = await GetGithubProfileFromJsonAsync(userJson);
+                await PrintProfileInfo(user);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static async Task<string> GetDataFromURLAsync(string url)
+        {
+            var webRequest = WebRequest.Create(url) as HttpWebRequest;
+
+            webRequest.ContentType = "application/json";
+            webRequest.UserAgent = "Nothing";
+
+            using (var s = webRequest.GetResponse().GetResponseStream())
+            {
+                using (var sr = new StreamReader(s))
+                {
+                    var contributorsAsJson = sr.ReadToEnd();
+                    return contributorsAsJson;
+                }
+            }
+        }
+
+        static async Task<GithubProfile> GetGithubProfileFromJsonAsync(string jsonString)
+        {
+            GithubProfile user = JsonConvert.DeserializeObject<GithubProfile>(jsonString);
+            return user;
+        }
+
+        static async Task<string> ReadUsernameFromConsoleAsync()
+        {
+            Console.Write("Please type the Github username: ");
+            string username = Console.ReadLine();
+
+            return username;
+        }
+
+        static async Task PrintProfileInfo(GithubProfile user)
+        {
+            Console.WriteLine($"Username: {user.login}");
+            Console.WriteLine($"Name: {user.name}");
+            Console.WriteLine($"Number of repositories: {user.public_repos}");
+            Console.WriteLine($"The number of followers: {user.followers}");
+            Console.WriteLine($"Follows: {user.following}");
+            Console.WriteLine($"Created account at: {user.created_at}");
+        }
+    }
+}
